814cdf4b4198065710988307058d70ba20c9b7c7
noraj <alexandre.zanni@europe.com>
2021-03-02 15:24:21 +0100

    pretty table class+ enhance table class
    - doc: works only for CSV::Table not array
    - add headers
    - pretty table formatter

--- lib/pass_station/output.rb
+++ lib/pass_station/output.rb
@@ -35,28 +35,28 @@ module PassStation
     class Table
       class << self
         # Format the +CSV::Table+ into a simple table with justified columns
-        # @param table [CSV::Table|Array<Array>] a +CSV::Table+ or any 2D array
+        # @param table [CSV::Table] a +CSV::Table+
         # @return [Array<String>] the formatted table ready to be printed
         def format(table)
           out = []
-          colsize = colsizes_count(table)
+          colsizes = colsizes_count(table)
+          out.push(headers(colsizes))
           table.each do |r|
-            out.push(justify_row(r, colsize))
+            out.push(justify_row(r, colsizes))
           end
           out
         end
 
         # Calculate column size (max item size)
-        # @param table [CSV::Table|Array<Array>]
-        # @param column [Symbol|Integer] the symbol of the column for +CSV::Table+ or the index integer for a classic
-        #   array
+        # @param table [CSV::Table]
+        # @param column [Symbol] the symbol of the column
         # @return [Integer] the column size
         def colsize_count(table, column)
-          table.map { |i| i[column].nil? ? 0 : i[column].size }.max
+          table.map { |i| i[column].nil? ? 0 : i[column].size }.max + 1
         end
 
         # Calculate the size of all columns (max item size)
-        # @param table [CSV::Table|Array<Array>]
+        # @param table [CSV::Table]
         # @return [Hash] keys are columns name, values are columns size
         def colsizes_count(table)
           table.first.to_h.keys.each_with_object({}) do |c, h|
@@ -65,9 +65,8 @@ module PassStation
         end
 
         # Left justify an element of the column
-        # @param row [CSV::Row|Array] +CSV::Row+ for a +CSV::Table+ or an array for a 2D array
-        # @param column [Symbol|Integer] the symbol of the column for +CSV::Table+ or the index integer for a classic
-        #   array
+        # @param row [CSV::Row] +CSV::Row+
+        # @param column [Symbol] the symbol of the column
         # @param colsizes [Hash] hash containing the column size for each column as returned by {colsizes_count}
         # @return [String] the justified element
         def justify(row, column, colsizes)
@@ -75,7 +74,7 @@ module PassStation
         end
 
         # Left justify all elements of the column
-        # @param row [CSV::Row|Array] +CSV::Row+ for a +CSV::Table+ or an array for a 2D array
+        # @param row [CSV::Row] +CSV::Row+
         # @param colsizes [Hash] hash containing the column size for each column as returned by {colsizes_count}
         # @return [String] the justified row
         def justify_row(row, colsizes)
@@ -86,7 +85,69 @@ module PassStation
           out
         end
 
-        protected :colsize_count, :colsizes_count, :justify, :justify_row
+        # Generate justified headers
+        # @param colsizes [Hash] hash containing the column size for each column as returned by {colsizes_count}
+        # @return [String] the justified headers
+        def headers(colsizes)
+          colsizes.map { |k, v| k.to_s.ljust(v) }.join.to_s
+        end
+
+        protected :colsize_count, :colsizes_count, :justify, :justify_row, :headers
+      end
+    end
+
+    # Pretty table with ASCII borders formatter
+    class PrettyTable < Table
+      class << self
+        # Format the +CSV::Table+ into a simple table with justified columns
+        # @param table [CSV::Table|Array<Array>] a +CSV::Table+ or any 2D array
+        # @return [Array<String>] the formatted table ready to be printed
+        def format(table)
+          out = []
+          colsizes = colsizes_count(table)
+          out.push(dividers(colsizes))
+          out.push(headers(colsizes))
+          out.push(dividers(colsizes))
+          table.each do |r|
+            out.push(justify_row(r, colsizes))
+          end
+          out.push(dividers(colsizes))
+        end
+
+        # Left justify an element of the column
+        # @param row [CSV::Row] +CSV::Row+
+        # @param column [Symbol] the symbol of the column
+        # @param colsizes [Hash] hash containing the column size for each column as returned by {colsizes_count}
+        # @return [String] the justified element
+        def justify(row, column, colsizes)
+          row[column].to_s.ljust(colsizes[column] - 1)
+        end
+
+        # Left justify all elements of the column
+        # @param row [CSV::Row] +CSV::Row+
+        # @param colsizes [Hash] hash containing the column size for each column as returned by {colsizes_count}
+        # @return [String] the justified row
+        def justify_row(row, colsizes)
+          out = '| '
+          row.to_h.each_key do |col|
+            out += "#{justify(row, col, colsizes)} | "
+          end
+          out
+        end
+
+        # Generate dividers
+        def dividers(colsizes)
+          "+#{colsizes.map { |_, cs| '-' * (cs + 1) }.join('+')}+"
+        end
+
+        # Generate justified headers
+        # @param colsizes [Hash] hash containing the column size for each column as returned by {colsizes_count}
+        # @return [String] the justified headers
+        def headers(colsizes)
+          "| #{colsizes.map { |k, v| k.to_s.ljust(v - 1) }.join(' | ')} |"
+        end
+
+        protected :dividers, :headers, :justify_row, :justify
       end
     end
   end
